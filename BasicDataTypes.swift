dd47999be2be32164c32b4657b172755baa2b982
Miguel Ángel Mtz Hdz <131746147+AngelMiguel1005@users.noreply.github.com>
2023-04-25 23:28:13 -0600

    Create BasicDataTypes.swift

---
+++ BasicDataTypes.swift
@@ -0,0 +1,68 @@
+//String
+let myString = "El gato come"
+let myString2 = "y le gusta dormir"
+let myString3 = myString + " " + myString2
+print(myString3)
+
+print("\n")
+
+//Int
+let myInt = 10
+let myInt2 = 5
+
+let myIntSuma = myInt + myInt2
+print(myIntSuma)
+
+let myIntResta = myInt - myInt2
+print(myIntResta)
+
+let myIntMulti = myInt * myInt2
+print(myIntMulti)
+
+let myIntDivi = myInt / myInt2
+print(myIntDivi)
+
+print("\n")
+
+//Double
+let myDouble = 5.5
+let myDouble2 = 2.1
+
+let myDoubleSuma = myDouble + myDouble2
+print(myDoubleSuma)
+
+let myDoubleResta = myDouble - myDouble2
+print(myDoubleResta)
+
+let myDoubleMulti = myDouble * myDouble2
+print(myDoubleMulti)
+
+let myDoubleDivi = myDouble / myDouble2
+print(myDoubleDivi)
+
+print("\n")
+
+//Float
+let myFloat: Float = 5.5
+let myFloat2: Float = 2.1
+
+let myFloatSuma = myFloat + myFloat2
+print(myFloatSuma)
+
+let myFloatResta = myFloat - myFloat2
+print(myFloatResta)
+
+let myFloatMulti = myFloat * myFloat2
+print(myFloatMulti)
+
+let myFloatDivi = myFloat / myFloat2
+print(myFloatDivi)
+
+print("\n")
+
+//Bool
+let myBool = false
+let myBool2 = true
+print(myBool == myBool2)
+print(myBool != myBool2)
+print(myBool && myBool2)
