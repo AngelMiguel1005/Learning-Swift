219a6885c9c6b55e5e043dc4003dfec25b7c1d1f
Miguel Ángel Mtz Hdz <131746147+AngelMiguel1005@users.noreply.github.com>
2023-04-26 07:00:08 -0600

    Create SentenceIf.swift

---
+++ SentenceIf.swift
@@ -0,0 +1,11 @@
+let myNumber = -10
+
+if (myNumber > 5 && myNumber < 10) || myNumber >= 50 {
+  print("\(myNumber) es mayor que 5 y menor que 10; o mayor o igual que 50")
+} else if myNumber == 1{
+  print("\(myNumber) es igual a 1")
+} else if myNumber == -10{
+  print("\(myNumber) es igual a -10")
+} else {
+  print("\(myNumber) es menor que 5 o es mayor que 10 pero menor a 50")
+}
