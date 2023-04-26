3ec802a509b318c82c5693e8f8c1e355c3dfc31c
Miguel Ángel Mtz Hdz <131746147+AngelMiguel1005@users.noreply.github.com>
2023-04-25 18:14:39 -0600

    Create variables&constantes.swift

---
+++ variables&constantes.swift
@@ -0,0 +1,32 @@
+//Variables
+
+var myFirstVariable = "Puro pinche Cruz Azul"
+print(myFirstVariable)
+
+var myFirstNumber = 10
+print(myFirstNumber)
+
+//que chingue su madre el América
+
+/*
+when haces tus momos en comentarios de swift
+*/
+
+var mySecondVariable = "Puro pinche Santi Giménez"
+print(mySecondVariable)
+
+mySecondVariable = myFirstVariable
+print(mySecondVariable)
+
+myFirstVariable = "Ehhhh putooooo"
+print(mySecondVariable)
+
+print("\n")
+
+//Constantes
+
+let myFirstConstant = "Messi > CR7"
+print(myFirstConstant)
+
+let mySecondConstant = myFirstVariable
+print(mySecondConstant)
